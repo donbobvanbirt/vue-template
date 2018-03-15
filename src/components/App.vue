5d090f20231727fd648ed9f34d9ff0f23a423627
Donovan <donovan6925@gmail.com>
2018-03-15 17:50:47 -0400

    updated text

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,7 +3,7 @@
         <sui-icon name="vuejs" />
         Vue Template
         <sui-header-subheader>
-            A simple boilerplate build with Vue.js, Webpack 4, and Semantic Ui
+            A simple boilerplate built with Vue.js, Webpack 4, and Semantic UI
         </sui-header-subheader>
     </h2>
 </template>
