7dc662d4869bff80c29c54ca540de3cc56f260a8
Donovan <donovan6925@gmail.com>
2018-03-15 16:48:54 -0400

    added semantic ui

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,14 @@
-<template>
-   <h1>Vue.js, Webpack 4 Template</h1>
+<template lang="html">
+    <h2 is="sui-header" icon>
+        <sui-icon name="vuejs" />
+        Vue Template
+        <sui-header-subheader>
+            A simple boilerplate build with Vue.js, Webpack 4, and Semantic Ui
+        </sui-header-subheader>
+    </h2>
 </template>
 <script>
-   export default {
-
-   }
+export default {
+  name: 'SizeExample'
+}
 </script>
