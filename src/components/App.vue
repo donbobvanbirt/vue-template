64eb84f17b7621e8692b33319dfd70ab6035ff95
Donovan <donovan6925@gmail.com>
2018-03-15 18:06:20 -0400

    added style

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,27 @@
 <template lang="html">
-    <h2 is="sui-header" icon>
-        <sui-icon name="vuejs" />
-        Vue Template
-        <sui-header-subheader>
-            A simple boilerplate built with Vue.js, Webpack 4, and Semantic UI
-        </sui-header-subheader>
-    </h2>
+    <div v-bind:style="containerStyle">
+        <h2 is="sui-header" icon>
+            <sui-icon name="vuejs" />
+            Vue Template
+            <sui-header-subheader>
+                A simple boilerplate built with Vue.js, Webpack 4, and Semantic UI
+            </sui-header-subheader>
+        </h2>
+    </div>
 </template>
 <script>
 export default {
-  name: 'SizeExample'
+  name: 'app',
+  data() {
+    return {
+      containerStyle: {
+        width: `100%`,
+        display: 'flex',
+        'justify-content': 'space-around',
+        'align-items': 'center',
+        height: '50%'
+      }
+    }
+  }
 }
 </script>
